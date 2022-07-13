49256a86edab65306c8d3adea474874f68877fbb
SetRedEyes <denidraven@gmail.com>
2022-07-13 23:18:05 +0200

    inplemented conversion logic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,72 @@
-import './App.css';
-
+import './App.css'
+import { useEffect, useState } from 'react'
+import CurrencyInput from './components/CurrencyInput'
+import axios from 'axios'
+import { takeCurrencyNames } from '../utils/takeСurrencyNames'
+import { convertCurrency } from '../utils/convertCurrency'
 
 const App = () => {
+  const [amount1, setAmount1] = useState(1)
+  const [amount2, setAmount2] = useState(1)
+  const [currency1, setCurrency1] = useState('usd')
+  const [currency2, setCurrency2] = useState('uah')
+  const [rates, setRates] = useState<{ [key: string]: number }>({})
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/uah.json'
+      )
+      .then((res) => {
+        const uah = res.data.uah
+        setRates(uah)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (rates) {
+      handleAmount1Change(1)
+    }
+  }, [rates])
+
+  const handleAmount1Change = (amount1: number) => {
+    setAmount2(convertCurrency(amount1, rates[currency2], rates[currency1]))
+    setAmount1(amount1)
+  }
+
+  const handleCurrency1Change = (currency1: string) => {
+    setAmount2(convertCurrency(amount1, rates[currency2], rates[currency1]))
+    setCurrency1(currency1)
+  }
+
+  const handleAmount2Change = (amount2: number) => {
+    setAmount1(convertCurrency(amount2, rates[currency1], rates[currency2]))
+    setAmount2(amount2)
+  }
+
+  const handleCurrency2Change = (currency2: string) => {
+    setAmount1(convertCurrency(amount2, rates[currency1], rates[currency2]))
+    setCurrency2(currency2)
+  }
+
   return (
     <div>
-      
+      <CurrencyInput
+        onAmountChange={handleAmount1Change}
+        onCurrencyChange={handleCurrency1Change}
+        currencyOpts={takeCurrencyNames(rates)}
+        amount={amount1}
+        currency={currency1}
+      />
+      <CurrencyInput
+        onAmountChange={handleAmount2Change}
+        onCurrencyChange={handleCurrency2Change}
+        currencyOpts={takeCurrencyNames(rates)}
+        amount={amount2}
+        currency={currency2}
+      />
     </div>
-  );
-};
+  )
+}
 
-
-export default App;
+export default App
--- src/app/components/CurrencyInput.tsx
+++ src/app/components/CurrencyInput.tsx
@@ -1,10 +1,44 @@
 
-const CurrencyInput = (props) => {
+export interface ICurrencyInputProps {
+  amount: number
+  currency: string
+  currencyOpts: string[]
+  onAmountChange: (target: number) => void
+  onCurrencyChange: (target: string) => void
+}
+
+const CurrencyInput = ({
+  amount,
+  currency,
+  currencyOpts,
+  onAmountChange,
+  onCurrencyChange
+}: ICurrencyInputProps) => {
+
+  const handleAmount= (e: React.ChangeEvent<HTMLInputElement>) => {
+  console.log(e.target.value)
+    onAmountChange(Number(e.target.value))
+  }
+
+  const handleCurrency =(e: React.ChangeEvent<HTMLSelectElement>) => {
+    onCurrencyChange(e.target.value)
+  }
+
   return (
     <div className='group'>
-      <input type="text" value={1} />
+      <input type='text' value={amount} onChange={handleAmount} />
+      <select
+        value={currency}
+        onChange={handleCurrency}
+      >
+        {currencyOpts.map((c) => (
+          <option key={c} value={c}>
+            {c.toUpperCase()}
+          </option>
+        ))}
+      </select>
     </div>
-  );
-};
+  )
+}
 
-export default CurrencyInput;
+export default CurrencyInput
